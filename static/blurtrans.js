da0749d01386184c1c086f1d35c55c88edd9b3eb
Corbin Matschull <c.matschull@icloud.com>
2017-07-18 15:14:44 -0600

    Pushed blurtrans.js refactor

--- static/blurtrans.js
+++ static/blurtrans.js
@@ -1,7 +1,6 @@
-(function($) {
+$(document).ready(function($) {
 	$(window).scroll((e) => {
 		e.preventDefault()
-
 		$(this).scrollTop() > 350
 			? $('.GHS_Image.blur-bg').addClass('active')
 			: $('.GHS_Image.blur-bg').removeClass('active')
